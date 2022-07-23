2bf784dba67572eae241dd017417a39a50d26a67
kkk <kkk@kkk.com>
2022-07-23 14:31:08 +0800

    master commit2

--- src/main/java/com/study/git/GitTest.java
+++ src/main/java/com/study/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println( "Hello Git222...");
         System.out.println( "Hello Git333...");
         System.out.println( "Hello Git444...");
+        System.out.println("Hello master...");
     }
 }
